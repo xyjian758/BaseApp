9442ae3f6fcc28fb340be22a4ff2df203e838702
xyjian758 <824006354@qq.com>
2017-04-06 17:10:07 +0800

    另一个用户改了点东西

--- app/src/main/java/com/example/app/demo/mvp/login/MainActivity.java
+++ app/src/main/java/com/example/app/demo/mvp/login/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends MvpActivity<MainPresenter, MainModel> implemen
     public void testView() {
         stopLoading();
 
+
         ARouter.getInstance()
                 .build(ArouteConstant.APP_OTHER)
                 .navigation();
